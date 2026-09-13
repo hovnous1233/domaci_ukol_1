d3749b3d4136d15cc897ea11faec95978986e869
hovnous1233 <vlhnatehvn@gmail.com>
2026-09-13 20:42:43 +0200

    prazdniny

---
+++ src/Main.java
@@ -0,0 +1,5 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    System.out.printf("Letošní prázdniny byly plné odpočinku. Nejprve jsem byl na brigádě s přáteli, abych co nejvíce vydělal. Potom jsem vyrazil na dovolenou do Polska, kde jsem byl na různých výletech . V druhé polovině prázdnin jsem odpočíval a trávil čas s přáteli. Nakonec jsem stihl zajít i párkrát do kina, na různé festival, letní akce a připravit se na nový školní rok.");
+}
